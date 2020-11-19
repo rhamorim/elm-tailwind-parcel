bcc5231aeb37284181058413eca50e7daa443365
Roberto Amorim <roberto.amorim@gmail.com>
2020-11-19 15:30:41 -0300

    Updating Tailwind to 2.0

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-
-const plugins =
-  process.env.NODE_ENV === 'production'
-    ? ['tailwindcss',
-       'autoprefixer',
-        purgecss({
-          content: [
-            './html/**.html',
-            './elm/**/*.elm'
-          ],
-          css: ['**/*.css'],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        })
-      ]
-    : ['tailwindcss'];
-
-module.exports = { plugins };
-
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
+  purge: [
+    './elm/**/*.elm',
+    './html/**/*.html',
+   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
 }
